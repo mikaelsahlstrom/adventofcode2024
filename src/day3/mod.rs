533a7035de9cb5def56547c03ea053c851b88d26
Mikael Sahlström <mick.sahlstrom@gmail.com>
2024-12-08 15:28:12 +0100

    Day 3 part 2.

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -33,5 +33,42 @@ pub fn part1()
 
 pub fn part2()
 {
+    let mut sum: u32 = 0;
 
+    if let Ok(lines) = utils::read_lines("src/day3/input")
+    {
+        let mut line: String = match lines.collect()
+        {
+            Ok(l) => l,
+            Err(_) => return
+        };
+
+        let instr_mul = regex::Regex::new(r"^mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+        let instr_do = regex::Regex::new(r"^do\(\)").unwrap();
+        let instr_dont = regex::Regex::new(r"^don't\(\)").unwrap();
+
+        let mut do_mul = true;
+        while !line.is_empty()
+        {
+            if instr_mul.is_match(&line) && do_mul
+            {
+                let caps = instr_mul.captures(&line).unwrap();
+                let a: u32 = caps[1].parse().unwrap();
+                let b: u32 = caps[2].parse().unwrap();
+                sum += a * b;
+            }
+            else if instr_do.is_match(&line)
+            {
+                do_mul = true;
+            }
+            else if instr_dont.is_match(&line)
+            {
+                do_mul = false;
+            }
+
+            line.remove(0);
+        }
+    }
+
+    println!("Part 2: {}", sum);
 }
